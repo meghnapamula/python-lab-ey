aef6251ca348e9b4736796ed55c54fda558ceed2
Elissa Yanez <Elissa.Yanez.25@cmc.edu>
2023-02-03 10:52:21 -0800

    attempt 1

--- Fixme.py
+++ Fixme.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 
-def evens(n):
-    '''
-    Returns a list of even numbers from 0 to n inclusive.
-    '''
+import Fixme
+
+import pytest
+
+def test__evens_1():
+    Fixme.evens(10) == [0, 2, 4, 6, 8, 10]
+
+
+def test__evens_2():
+     Fixme.evens(11) == [0, 2, 4, 6, 8, 10]
+
+
+def test__evens_3():
+     Fixme.evens(0) == [0]
+
+
+def test__evens_4():
+     Fixme.evens(1) == [0]
+
+
+def test__evens_5():
+    Fixme.evens(-1) == []
+    
