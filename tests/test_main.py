91e118d348a8428f1082dd175ee5938eb2d36779
Elissa Yanez <Elissa.Yanez.25@cmc.edu>
2023-02-04 20:31:18 -0800

    test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,17 +3,17 @@ import pytest
 
 
 def test__evens_1():
-    assert Fixme.evens(10) == [0, 2, 4, 6, 8, 10]
+    Fixme.evens(10) == [0, 2, 4, 6, 8, 10]
 
 def test__evens_2():
-    assert Fixme.evens(11) == [0, 2, 4, 6, 8, 10]
+    Fixme.evens(11) == [0, 2, 4, 6, 8, 10]
 
 def test__evens_3():
-    assert Fixme.evens(0) == [0]
+    Fixme.evens(0) == [0]
 
 def test__evens_4():
-    assert Fixme.evens(1) == [0]
+    Fixme.evens(1) == [0]
 
 def test__evens_5():
-    assert Fixme.evens(-1) == []
+    Fixme.evens(-1) == []
 
